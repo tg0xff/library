94806e40cf170384e2233bb7c5cd4e053d2c5d56
tg0xff <tg0xff@proton.me>
2024-05-16 11:38:25 -0600

    Define displayBooks()

--- main.js
+++ main.js
@@ -21,3 +21,34 @@ function addBookToLibrary() {
   const book = Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+function displayBooks() {
+  const body = document.querySelector("body");
+  const table = document.createElement("table");
+
+  const thead = document.createElement("thead");
+  const theadRow = document.createElement("tr");
+  const tableColumns = ["Title", "Author", "# of pages", "Read"];
+  for (const column of tableColumns) {
+    const th = document.createElement("th");
+    th.setAttribute("scope", "col");
+    th.textContent = column;
+    theadRow.appendChild(th);
+  }
+  thead.appendChild(theadRow);
+  table.appendChild(thead);
+
+  const tbody = document.createElement("tbody");
+  for (const book of myLibrary) {
+    const tbodyRow = document.createElement("tr");
+    const bookData = Object.values(book);
+    for (const datum of bookData) {
+      const td = document.createElement("td");
+      td.textContent = datum;
+      tbodyRow.appendChild(td);
+    }
+    tbody.appendChild(tbodyRow);
+  }
+  table.appendChild(tbody);
+  body.appendChild(table);
+}
