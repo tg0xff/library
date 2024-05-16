768c0e4ba5a970f490fa917fb9627be7aed8311a
tg0xff <tg0xff@proton.me>
2024-05-16 11:27:50 -0600

    Define addBookToLibrary()

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   self.title = title;
   self.author = author;
@@ -8,3 +10,14 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function () {
   return `${self.title} by ${self.author}, ${self.pages} pages, ${read ? "Read" : "Not read yet"}`;
 };
+
+function addBookToLibrary() {
+  const title = prompt("Title: ");
+  const author = prompt("Author: ");
+  const pages = prompt("Pages: ");
+  let read = prompt("Read: ");
+  read = read.toLowerCase();
+  read = read === "y";
+  const book = Book(title, author, pages, read);
+  myLibrary.push(book);
+}
