abccc1e11ea0984ab0c7d983663f5f1b637f02d6
tg0xff <tg0xff@proton.me>
2024-06-05 18:48:18 -0600

    Turn Book constructor into a class

--- main.js
+++ main.js
@@ -41,19 +41,19 @@ function main(e) {
   }
 }
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "Read" : "Not read yet"}`;
-};
-
-Book.prototype.changeRead = function () {
-  this.read = !this.read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "Read" : "Not read yet"}`;
+  }
+  changeRead() {
+    this.read = !this.read;
+  }
 }
 
 function addBookToLibrary() {
